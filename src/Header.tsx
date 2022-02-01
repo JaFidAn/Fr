e8403b313623b4fa9fc298869626937a0d174bd0
Rasim Alagezov <r.alagezov@gmail.com>
2022-02-01 23:28:34 +0400

    01.02.2022-3

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
-export const Header = () => (
-  <div>
-    <a href="./">Q & A</a>
-    <input type="text" placeholder="Search..." />
-    <a href='./signin'><span>Sign in</span></a>
-  </div>
-)
+import React from 'react';
+
+
+export const Header = () => {
+  const handleSearchInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.currentTarget.value);
+  };
+  return (
+    <div>
+      <a href="./">Q & A</a>
+      <input
+        type="text"
+        placeholder="Search..."
+        onChange={handleSearchInputChange}
+      />
+      <a href="./signin">
+        <span>Sign In</span>
+      </a>
+    </div>
+  );
+};

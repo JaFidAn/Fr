b4280aeda9b5058756f9b050af85ec815e412d3c
Rasim Alagezov <r.alagezov@gmail.com>
2022-02-01 13:10:50 +0400

    FrontendRact

---
+++ src/Header.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+export const Header = () => (
+  <div>
+    <a href="./">Q & A</a>
+    <input type="text" placeholder="Search..." />
+    <a href='./signin'><span>Sign in</span></a>
+  </div>
+)
